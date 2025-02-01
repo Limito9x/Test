6d053cb1649c069db0168c5151619d5acc2fd6cf
Limito9x <limito9x@gmail.com>
2025-02-01 17:19:58 +0700

    2025 - v4

--- src/server.js
+++ src/server.js
@@ -5,18 +5,18 @@ const app = express()
 const port = process.env.PORT
 const hostname = process.env.HOST_NAME
 
-app.use(express.static(path.join(__dirname, 'tempfolder'))) //Cau hinh thu muc tinh
-app.set('views', path.join(__dirname,'view')) // Tao template ben trong thu muc view (src/view)
+app.use(express.static(path.join(__dirname, 'public'))) //Cau hinh thu muc tinh
+app.set('views', path.join(__dirname, 'view')) // Tao template ben trong thu muc view (src/view)
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! I am Thuan')
 })
 app.get('/ejstest', (req, res) => {
   res.render('sample.ejs')
 })
 
 
-app.listen(port,hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port} ${hostname}`)
 })
